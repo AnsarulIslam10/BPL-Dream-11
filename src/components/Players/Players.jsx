3b4c72c4f23b0e228c85b68dbb1748be8b9c0d3f
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-25 00:24:53 +0600

    button toggle design

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export default function Players() {
   const [players, setPlayers] = useState([]);
-
+  const [tab, setTab] = useState('available')
   useEffect(() => {
     fetch("./players.json")
       .then((res) => res.json())
@@ -16,9 +16,10 @@ export default function Players() {
       <div className="flex justify-between items-center">
         <h2 className="text-2xl font-bold">Available Players</h2>
 
-        <div className="px-4 py-2 border rounded-xl flex gap-2">
-          <button className="">Available</button>
-          <button className="">Selectet (0)</button>
+        <div className="border rounded-xl flex">
+          <button onClick={() => setTab('available')} className={'px-4 rounded-tl-xl py-2 rounded-bl-xl duration-200 font-medium' + (tab === 'available' ? ' bg-[#E7FE29] text-black' : '')}>Available</button>
+
+          <button onClick={() => setTab('selected')} className={'px-4 duration-200 py-2 rounded-tr-xl rounded-br-xl font-medium' + (tab === 'selected' ? ' bg-[#E7FE29] text-black' : '')} >Selectet (0)</button>
         </div>
       </div>
 
