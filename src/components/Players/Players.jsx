7ad2c837410b202c236734e5f6e7ce6dab0ecfc7
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-24 22:51:29 +0600

    adding cards by fetching json

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -2,13 +2,14 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 export default function Players() {
-    const [players, setPlayers] = useState([])
+  const [players, setPlayers] = useState([]);
 
-    useEffect(()=>{
-        fetch('./players.json')
-        .then(res=>res.json())
-        .then(data=>console.log(data))
-    },[])
+  useEffect(() => {
+    fetch("./players.json")
+      .then((res) => res.json())
+      .then((data) => setPlayers(data));
+  }, []);
+  console.log(setPlayers)
 
   return (
     <div className="mt-6">
@@ -22,7 +23,49 @@ export default function Players() {
       </div>
 
       {/* players card */}
-
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
+        {
+         players.map((player) => (
+          <div
+            key={player.playerId}
+            className="card bg-base-100 shadow-xl"
+          >
+            <figure className="px-10 pt-10 h-72">
+              <img
+                src={player.image}
+                alt="Shoes"
+                className="rounded-xl w-full h-full object-cover"
+              />
+            </figure>
+            <div className="card-body">
+              <div className="flex items-center gap-2">
+                <i className="fa-solid fa-user text-xl"></i>
+                <h2 className="card-title">{player.name}</h2>
+              </div>
+              <div className="flex items-center justify-between pb-2 border-b-2">
+                <div className="flex items-center gap-2 text-gray-400">
+                  <i className="fa-solid fa-flag"></i>
+                  <h3>{player.country}</h3>
+                </div>
+                <h3 className="px-4 py-2 bg-gray-200 rounded-xl">{player.role}</h3>
+              </div>
+              <div>
+                <h3 className="text-lg font-semibold mb-3">Rating</h3>
+                <div className="flex justify-between items-center text-xl mb-4">
+                  <h3>{player.battingType}</h3>
+                  <h3>{player.bowlingType}</h3>
+                </div>
+                <div className="flex justify-between items-center text-lg">
+                  <h3>Price: ${player.biddingPrice}</h3>
+                  <button className="btn btn-outline border-gray-200">
+                    Choose Player
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
